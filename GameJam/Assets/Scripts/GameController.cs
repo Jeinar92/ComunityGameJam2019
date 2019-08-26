623c09f7bdc40959e6ec64230196b2ca2b261139
Jeinar <ononb1992@gmail.com>
2019-08-26 16:16:34 +0200

    Create character

--- GameJam/Assets/Scripts/GameController.cs
+++ GameJam/Assets/Scripts/GameController.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
-{
+{   [Range (0f, 0.2f)]
+    [SerializeField] float parallaxSpeed = 0.02f;
+    [SerializeField] float characterSpeed = 10f;
+    public RawImage background;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,8 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float finalCharacterSpeed = characterSpeed * Time.deltaTime;
+        float finalSpeed =characterSpeed * parallaxSpeed * Time.deltaTime;
+        background.uvRect = new Rect(background.uvRect.x + finalSpeed, 0f, 1f, 1f);
     }
 }
