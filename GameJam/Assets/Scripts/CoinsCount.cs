18808779134e1c6258f3dc6256b2f560ad8f76a5
Jeinar <ononb1992@gmail.com>
2019-08-27 18:57:39 +0200

    Add coin Count

---
+++ GameJam/Assets/Scripts/CoinsCount.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinsCount : MonoBehaviour
+{
+    [SerializeField] int numberOfHonest = 0;
+    [SerializeField] int numberOfCoins;
+    [SerializeField] int currentCoins;
+    [SerializeField] bool added = false;
+
+     void Start()
+    {
+        currentCoins = (numberOfHonest * 1000 )- (numberOfHonest -1) ;
+        Debug.Log(numberOfHonest);
+    }
+
+    private void Update()
+    {
+        if (added == true)
+        {
+            Debug.Log("Current Coins" + currentCoins);
+            added = false;
+        }
+        numberOfCoins = numberOfHonest;
+    }
+    public void HonestSum()
+    {
+        numberOfHonest ++;
+        added = true;
+        Debug.Log(numberOfHonest);
+    }
+    public void HonestSubstract()
+    {
+        numberOfHonest --;
+        
+        Debug.Log(numberOfHonest);
+    }
+
+
+}
