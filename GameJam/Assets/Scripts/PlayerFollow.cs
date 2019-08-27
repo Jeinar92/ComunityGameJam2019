dc75b455ead9adaab22ef9459835543b094b773b
Jeinar <ononb1992@gmail.com>
2019-08-28 00:22:21 +0200

    meh

---
+++ GameJam/Assets/Scripts/PlayerFollow.cs
@@ -0,0 +1,17 @@
+﻿
+using UnityEngine;
+
+public class PlayerFollow : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [Range (0,10)][SerializeField] float smoothSpeed =10f;
+    [SerializeField] Vector3 offSet;
+
+    void LateUpdate()
+    {
+        Vector3 desiredPosition = target.position + offSet;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+        transform.position = smoothedPosition;
+    }
+
+}
