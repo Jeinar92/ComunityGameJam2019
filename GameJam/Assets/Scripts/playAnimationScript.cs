64fa741a1cbb73a3ef24aea9c4aecf54fcb8f652
Jeinar <ononb1992@gmail.com>
2019-08-31 06:22:45 +0200

    up

---
+++ GameJam/Assets/Scripts/playAnimationScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playAnimationScript : MonoBehaviour
+{
+
+    [SerializeField] Animation yourAnimation;
+
+    
+    // This is an example only
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            yourAnimation.Play();
+        }
+    }
+}
